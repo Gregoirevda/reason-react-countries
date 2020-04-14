7fde3f5377a0ffd2de8c4188276560c618eaf090
Grégoire Van der Auwermeulen <gregoirevandera@gmail.com>
2020-04-14 13:49:21 +0200

    Delete .bs.js files

--- src/Index.bs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require("react");
-var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
-var App$CountriesList = require("./App.bs.js");
-
-ReactDOMRe.renderToElementWithId(React.createElement(App$CountriesList.make, { }), "root");
-
-/*  Not a pure module */
